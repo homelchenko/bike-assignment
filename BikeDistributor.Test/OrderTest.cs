86c0432a3842a7aee553e61bba3c964063139200
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2018-06-11 09:38:50 +0300

    Rename unit tests and add some explicit structure

--- BikeDistributor.Test/OrderTest.cs
+++ BikeDistributor.Test/OrderTest.cs
@@ -10,76 +10,112 @@ namespace BikeDistributor.Test
         private readonly static Bike DuraAce = new Bike("Specialized", "S-Works Venge Dura-Ace", Bike.FiveThousand);
 
         [TestMethod]
-        public void ReceiptOneDefy()
+        public void Receipt_WhenOrderContainsOneDefy_ShouldGenerateProperTextReceipt()
         {
+            // Arrange
             var order = new Order("Anywhere Bike Shop");
             order.AddLine(new Line(Defy, 1));
-            Assert.AreEqual(ResultStatementOneDefy, order.Receipt());
-        }
+            
+            // Act
+            string textReceipt = order.Receipt();
 
-        private const string ResultStatementOneDefy = @"Order Receipt for Anywhere Bike Shop
+            // Assert
+            Assert.AreEqual(
+@"Order Receipt for Anywhere Bike Shop
 	1 x Giant Defy 1 = $1,000.00
 Sub-Total: $1,000.00
 Tax: $72.50
-Total: $1,072.50";
-
-        [TestMethod]
-        public void ReceiptOneElite()
-        {
-            var order = new Order("Anywhere Bike Shop");
-            order.AddLine(new Line(Elite, 1));
-            Assert.AreEqual(ResultStatementOneElite, order.Receipt());
+Total: $1,072.50",
+                textReceipt);
         }
 
-        private const string ResultStatementOneElite = @"Order Receipt for Anywhere Bike Shop
+        [TestMethod]
+        public void Receipt_WhenOrderContainsOneElite_ShouldGenerateProperTextReceipt()
+        {
+            // Arrange
+            var order = new Order("Anywhere Bike Shop");
+            order.AddLine(new Line(Elite, 1));
+            
+            // Act
+            string textReceive = order.Receipt();
+
+            // Assert
+            Assert.AreEqual(
+@"Order Receipt for Anywhere Bike Shop
 	1 x Specialized Venge Elite = $2,000.00
 Sub-Total: $2,000.00
 Tax: $145.00
-Total: $2,145.00";
-
-        [TestMethod]
-        public void ReceiptOneDuraAce()
-        {
-            var order = new Order("Anywhere Bike Shop");
-            order.AddLine(new Line(DuraAce, 1));
-            Assert.AreEqual(ResultStatementOneDuraAce, order.Receipt());
+Total: $2,145.00",
+                textReceive);
         }
 
-        private const string ResultStatementOneDuraAce = @"Order Receipt for Anywhere Bike Shop
+        [TestMethod]
+        public void Receipt_WhenOrderContainsOneDuraAce_ShouldGenerateProperTextReceipt()
+        {
+            // Arrange
+            var order = new Order("Anywhere Bike Shop");
+            order.AddLine(new Line(DuraAce, 1));
+            
+            // Act
+            string textReceipt = order.Receipt();
+
+            // Arrange
+            Assert.AreEqual(
+@"Order Receipt for Anywhere Bike Shop
 	1 x Specialized S-Works Venge Dura-Ace = $5,000.00
 Sub-Total: $5,000.00
 Tax: $362.50
-Total: $5,362.50";
+Total: $5,362.50",
+                textReceipt);
+        }
 
         [TestMethod]
-        public void HtmlReceiptOneDefy()
+        public void Receipt_WhenOrderContainsOneDefy_ShouldGenerateProperHtmlReceipt()
         {
+            // Arrange
             var order = new Order("Anywhere Bike Shop");
             order.AddLine(new Line(Defy, 1));
-            Assert.AreEqual(HtmlResultStatementOneDefy, order.HtmlReceipt());
+            
+            // Act
+            string htmlReceipt = order.HtmlReceipt();
+
+            // Assert
+            Assert.AreEqual(
+                @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Giant Defy 1 = $1,000.00</li></ul><h3>Sub-Total: $1,000.00</h3><h3>Tax: $72.50</h3><h2>Total: $1,072.50</h2></body></html>",
+                htmlReceipt);
         }
 
-        private const string HtmlResultStatementOneDefy = @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Giant Defy 1 = $1,000.00</li></ul><h3>Sub-Total: $1,000.00</h3><h3>Tax: $72.50</h3><h2>Total: $1,072.50</h2></body></html>";
-
         [TestMethod]
-        public void HtmlReceiptOneElite()
+        public void Receipt_WhenOrderContainsOneElite_ShouldGenerateProperHtmlReceipt()
         {
+            // Arrange
             var order = new Order("Anywhere Bike Shop");
             order.AddLine(new Line(Elite, 1));
-            Assert.AreEqual(HtmlResultStatementOneElite, order.HtmlReceipt());
-        }
+            
+            // Act
+            string htmlReceipt = order.HtmlReceipt();
 
-        private const string HtmlResultStatementOneElite = @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Specialized Venge Elite = $2,000.00</li></ul><h3>Sub-Total: $2,000.00</h3><h3>Tax: $145.00</h3><h2>Total: $2,145.00</h2></body></html>";
+            // Assert
+            Assert.AreEqual(
+                @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Specialized Venge Elite = $2,000.00</li></ul><h3>Sub-Total: $2,000.00</h3><h3>Tax: $145.00</h3><h2>Total: $2,145.00</h2></body></html>",
+                htmlReceipt);
+        }
 
         [TestMethod]
-        public void HtmlReceiptOneDuraAce()
+        public void Receipt_WhenOrderContainsOneDuraAce_ShouldGenerateProperHtmlReceipt()
         {
+            // Arrange
             var order = new Order("Anywhere Bike Shop");
             order.AddLine(new Line(DuraAce, 1));
-            Assert.AreEqual(HtmlResultStatementOneDuraAce, order.HtmlReceipt());
-        }
 
-        private const string HtmlResultStatementOneDuraAce = @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Specialized S-Works Venge Dura-Ace = $5,000.00</li></ul><h3>Sub-Total: $5,000.00</h3><h3>Tax: $362.50</h3><h2>Total: $5,362.50</h2></body></html>";    
+            // Act
+            string htmlReceipt = order.HtmlReceipt();
+
+            // Arrange
+            Assert.AreEqual(
+                @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Specialized S-Works Venge Dura-Ace = $5,000.00</li></ul><h3>Sub-Total: $5,000.00</h3><h3>Tax: $362.50</h3><h2>Total: $5,362.50</h2></body></html>",
+                htmlReceipt);
+        }
     }
 }
 
