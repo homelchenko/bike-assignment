e7886fb3869e317dadf6f102212efe990a38e133
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2018-06-11 15:13:27 +0300

    Extract text receipt builder class into separate file

--- BikeDistributor/ReceiptBuilder.cs
+++ BikeDistributor/ReceiptBuilder.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Text;
-
-namespace BikeDistributor
+﻿namespace BikeDistributor
 {
     internal abstract class ReceiptBuilder
     {
@@ -14,47 +11,5 @@ namespace BikeDistributor
         public abstract void AddTaxSection(double tax);
 
         public abstract void AddTotalSection(double total);
-    }
-
-    internal class TextReceiptBuilder : ReceiptBuilder
-    {
-        private readonly StringBuilder _receipt = new StringBuilder();
-
-        public override void AddHeader(string company)
-        {
-            string receiptHeader = $"Order Receipt for {company}{Environment.NewLine}";
-
-            _receipt.Append(receiptHeader);
-        }
-
-        public override void AddLineItemSection(Line line, double lineItemTotal)
-        {
-            string lineItem = $"\t{line.Quantity} x {line.Bike.Brand} {line.Bike.Model} = {lineItemTotal:C}";
-            
-            _receipt.AppendLine(lineItem);
-        }
-
-        public override void AddSubTotalSection(double subTotal)
-        {
-            string subTotalSection = $"Sub-Total: {subTotal:C}";
-
-            _receipt.AppendLine(subTotalSection);
-        }
-
-        public override void AddTaxSection(double tax)
-        {
-            string taxSection = $"Tax: {tax:C}";
-
-            _receipt.AppendLine(taxSection);
-        }
-
-        public override void AddTotalSection(double total)
-        {
-            string totalSection = $"Total: {total:C}";
-
-            _receipt.Append(totalSection);
-        }
-
-        public string GetReceipt() => _receipt.ToString();
     }
 }
